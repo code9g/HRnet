36b145ad780a8167dc0301201b926079c1ba16dc
Pierre HENRY <pierre.henry@code99.fr>
2024-09-11 00:16:51 +0200

    correction json

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <footer>
-      <div className="container mx-auto py-4 text-center text-primary dark:text-green-100 font-bold">
-        Copyright (c) Wealth Health 2024
+      <div className="container mx-auto py-4 text-center text-green-900 dark:text-green-100 font-bold">
+        Copyright (c) Wealth Health {new Date().getFullYear()}
       </div>
     </footer>
   );
