07920a7dd92199e30790190ea8f7873bc828848b
Pierre HENRY <pierre.henry@code99.fr>
2024-09-10 11:07:28 +0200

    Home + logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import logo from "../assets/logo.png";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center gap-16">
+      <h1 className="text-center text-3xl font-bold uppercase text-primary">
+        Wealth Health
+      </h1>
+      <img src={logo} alt="logo" width="300" height="300" />
+    </div>
+  );
 }
 
 Home.propTypes = {};
