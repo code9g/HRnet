9fd4458c84b542082e93b725a12d50f537b22d3b
Pierre HENRY <pierre.henry@code99.fr>
2024-09-13 22:49:16 +0200

    Page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,10 @@
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex flex-col gap-10 text-3xl justify-center items-center py-20">
+      <h2 className="text-6xl text-red-800 dark:text-red-100">404</h2>
+      <p className="">Sorry, didn&apos;t found this page.</p>
+    </div>
+  );
 }
 
 NotFound.propTypes = {};
