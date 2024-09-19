4cf9c95573caaa6ff83c5a33a472090aac30a7bd
Pierre HENRY <pierre.henry@code99.fr>
2024-09-19 14:37:44 +0200

    Ajout du hover sur State

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -6,6 +6,9 @@ import {
 
 import DataTable from "@/components/DataTable/DataTable";
 import DataTableColumnHeader from "@/components/DataTable/DataTableColumnHeader";
+import { Button } from "@/components/ui/button";
+import { HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { HoverCard } from "@radix-ui/react-hover-card";
 
 const intlDTF = new Intl.DateTimeFormat("en-US", {
   year: "numeric",
@@ -13,77 +16,93 @@ const intlDTF = new Intl.DateTimeFormat("en-US", {
   day: "2-digit",
 });
 
-const columns = [
-  {
-    title: "First Name",
-    accessorKey: "firstName",
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-    filterFn: (row, id, value) => {
-      value = value.toLowerCase();
-      return (
-        row.getValue("firstName").toLowerCase().includes(value) ||
-        row.getValue("lastName").toLowerCase().includes(value) ||
-        row.getValue("street").toLowerCase().includes(value) ||
-        row.getValue("city").toLowerCase().includes(value)
-      );
-    },
-  },
-  {
-    title: "Last Name",
-    accessorKey: "lastName",
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-  },
-  {
-    title: "Date of Birth",
-    accessorKey: "dateOfBirth",
-    className: { header: "justify-center", cell: "text-center" },
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-    cell: ({ row, column }) =>
-      intlDTF.format(new Date(row.getValue(column.columnDef.accessorKey))),
-  },
-  {
-    title: "Start Date",
-    accessorKey: "startDate",
-    className: { header: "justify-center", cell: "text-center" },
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-    cell: ({ row, column }) =>
-      intlDTF.format(new Date(row.getValue(column.columnDef.accessorKey))),
-  },
-  {
-    title: "Street",
-    accessorKey: "street",
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-  },
-  {
-    title: "City",
-    accessorKey: "city",
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-  },
-  {
-    title: "State",
-    accessorKey: "state",
-    className: { header: "justify-center", cell: "text-center" },
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-    filterFn: (row, id, value) => value.includes(row.getValue(id)),
-  },
-  {
-    title: "Zip code",
-    accessorKey: "zipCode",
-    className: { header: "justify-center", cell: "text-center" },
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-  },
-  {
-    title: "Department",
-    accessorKey: "department",
-    header: ({ column }) => <DataTableColumnHeader column={column} />,
-    filterFn: (row, id, value) => value.includes(row.getValue(id)),
-  },
-];
-
 function EmployeeList() {
   const employees = useEmployeesSelector();
   const departments = useDepartmentsSelector();
   const states = useStatesSelector();
+
+  const columns = [
+    {
+      title: "First Name",
+      accessorKey: "firstName",
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+      filterFn: (row, id, value) => {
+        value = value.toLowerCase();
+        return (
+          row.getValue("firstName").toLowerCase().includes(value) ||
+          row.getValue("lastName").toLowerCase().includes(value) ||
+          row.getValue("street").toLowerCase().includes(value) ||
+          row.getValue("city").toLowerCase().includes(value)
+        );
+      },
+    },
+    {
+      title: "Last Name",
+      accessorKey: "lastName",
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+    },
+    {
+      title: "Date of Birth",
+      accessorKey: "dateOfBirth",
+      className: { header: "justify-center", cell: "text-center" },
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+      cell: ({ row, column }) =>
+        intlDTF.format(new Date(row.getValue(column.columnDef.accessorKey))),
+    },
+    {
+      title: "Start Date",
+      accessorKey: "startDate",
+      className: { header: "justify-center", cell: "text-center" },
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+      cell: ({ row, column }) =>
+        intlDTF.format(new Date(row.getValue(column.columnDef.accessorKey))),
+    },
+    {
+      title: "Street",
+      accessorKey: "street",
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+    },
+    {
+      title: "City",
+      accessorKey: "city",
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+    },
+    {
+      title: "State",
+      accessorKey: "state",
+      className: { header: "justify-center", cell: "text-center" },
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+      cell: ({ row, column }) => {
+        const value = row.getValue(column.id);
+        const state = states.find((item) => item.abbreviation === value);
+        return (
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <Button variant="none" className="font-normal">
+                {value}
+              </Button>
+            </HoverCardTrigger>
+            <HoverCardContent className="flex justify-center items-center w-fit space-x-4">
+              {state.name} ({state.abbreviation})
+            </HoverCardContent>
+          </HoverCard>
+        );
+      },
+      filterFn: (row, id, value) => value.includes(row.getValue(id)),
+    },
+    {
+      title: "Zip code",
+      accessorKey: "zipCode",
+      className: { header: "justify-center", cell: "text-center" },
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+    },
+    {
+      title: "Department",
+      accessorKey: "department",
+      header: ({ column }) => <DataTableColumnHeader column={column} />,
+      filterFn: (row, id, value) => value.includes(row.getValue(id)),
+    },
+  ];
 
   const filters = [
     {
@@ -116,7 +135,5 @@ function EmployeeList() {
     </>
   );
 }
-
-EmployeeList.propTypes = {};
 
 export default EmployeeList;
