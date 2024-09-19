ca8fd929a3570e1fb9975366c3a8a7ab9271b7be
Pierre HENRY <pierre.henry@code99.fr>
2024-09-19 12:58:53 +0200

    Changement taille du titre

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 function Home() {
   return (
     <div className="flex flex-col items-center gap-16">
-      <h1 className="text-center text-3xl font-medium">Wealth Health</h1>
+      <h1 className="text-center text-6xl font-medium">Wealth Health</h1>
       <img src={logo} alt="logo" width="300" height="300" />
     </div>
   );
