8567d13ee18ea6565053daaf67a9ced451d78b0e
Pierre HENRY <pierre.henry@code99.fr>
2024-11-05 15:22:09 +0100

    add welcome message on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ function Home() {
     <div className="flex flex-col items-center gap-16">
       <h1 className="text-center text-6xl font-medium">Wealth Health</h1>
       <img src={logo} alt="logo" width="300" height="300" />
+      <p className="text-center text-xl font-thin">
+        Welcome to HRnet! This is our company&apos;s internal application to
+        create and view employee records
+      </p>
     </div>
   );
 }
